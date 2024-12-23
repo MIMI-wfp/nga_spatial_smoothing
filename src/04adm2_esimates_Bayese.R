7d1b385ee52e11dae7883f4e628fd0b3e8c2c058
Sahoko Ishida <sahoko.ishida@gmail.com>
2024-12-23 19:43:27 +0900

    full  bayes model

--- src/04adm2_esimates_Bayese.R
+++ src/04adm2_esimates_Bayese.R
@@ -4,7 +4,7 @@
 
 # Author: Sahoko Ishida
 # Date created: 07-11-2024
-# Last edited: 11-11-2024
+# Last edited: 09-12-2024
 
 # In this script, I will generate both smoothed ADM2 level estimates
 # for inadequate intake of vitamin B12 in Nigeria. 
@@ -15,7 +15,7 @@
 
 # INSTALL AND LOAD PACKAGES:
 
-rq_packages <- c("readr", "tidyverse", "srvyr", "sf", "spdep", "tmap", "INLA", 
+rq_packages <- c("readr", "readxl","tidyverse", "srvyr", "sf", "spdep", "tmap", "INLA", 
                  "SUMMER", "wesanderson", "cmdstanr", "ggplot2")
 
 installed_packages <- rq_packages %in% rownames(installed.packages())
@@ -33,6 +33,7 @@ rm(list= c("rq_packages", "installed_packages"))
 
 # Read in pre-processed indicators:
 source("src/02indicator_preprocessing.R")
+source("src/helper_functions.R")
 
 # Read in household locations that are matched to shapefiles: 
 household_locations <- read_csv("shapefiles/household_locations.csv")
@@ -84,10 +85,35 @@ vb12_inad_lga <- vb12_inad_lga %>%
 vb12_inad_lga <- vb12_inad_lga |> 
   left_join(
     (nga_analysis_df |> group_by(lga, ea) |> 
-      summarise(n=n()) |> group_by(lga) |> 
-      summarise(d = n()-1, k = sum(n)) |> ungroup()),
+       summarise(n=n()) |> group_by(lga) |> 
+       summarise(d = n()-1, k = sum(n)) |> ungroup()),
     by = "lga"
   )
+
+# Calculate variance & degree of freedom of weighted direct estimates (ignoring survey design)
+## standardise weight
+nga_analysis_df <- nga_analysis_df |> 
+  left_join((nga_analysis_df|>
+              group_by(lga) |>
+              reframe(hhid = hhid,
+                      survey_wgt=survey_wgt,
+                      sum_wgt = sum(survey_wgt),
+                      norm_survey_wgt =survey_wgt/sum_wgt) |>
+               select(-lga, -survey_wgt,-sum_wgt)),
+            by = 'hhid'
+            ) 
+
+
+## calculate weighted proportion, (unbiased) variance of the estimate and degree of freedom
+vb12_inad_lga <- vb12_inad_lga |> 
+  left_join( nga_analysis_df |> group_by(lga) |>
+               summarise(n_obs = n(),
+                         n_eff = 1/sum(norm_survey_wgt^2),
+                         degf = (n_eff-1),
+                         vb12_inad_wdir = weighted.mean(vb12_inadequate, norm_survey_wgt),
+                         vb12_inad_wdir_var = vb12_inad_wdir*(1-vb12_inad_wdir)/degf),
+             by = 'lga'
+             )
 
 # Give adm2_index
 nigeria_2$lga_id <- 1:nrow(nigeria_2)#as.integer(as.factor(nigeria_2$lga))
@@ -117,23 +143,38 @@ for (i in 1:length(prep$n1)){
 }
 mean(qc) # shuold be 1
 
-vb12_inad_lga_complete <- vb12_inad_lga |> filter(!is.na(vb12_inad_dir_var),d!=0,vb12_inad_dir_var>1e-10)
-# read stan file
+#vb12_inad_lga_complete <- vb12_inad_lga |> filter(!is.na(vb12_inad_dir_var),d!=0,vb12_inad_dir_var>1e-10)
+vb12_inad_lga_complete2 <- vb12_inad_lga |> filter(!is.na(vb12_inad_wdir_var),degf!=0,vb12_inad_wdir_var>1e-10) 
+
 stanfile <- here('src','areal_level_BYM2.stan')
 mod <- cmdstan_model(stanfile)
+# data_list <- list(
+#   N = nrow(vb12_inad_lga),
+#   NS = nrow(vb12_inad_lga_complete),
+#   adm2_index = vb12_inad_lga_complete$lga_id,
+#   p_hat = vb12_inad_lga_complete$vb12_inad_dir,
+#   v_hat = vb12_inad_lga_complete$vb12_inad_dir_var,
+#   d = vb12_inad_lga_complete$d, 
+#   k = vb12_inad_lga_complete$k,
+#   N_edges = length(prep$n1),
+#   node1 = prep$n1,
+#   node2 = prep$n2,
+#   scaling_factor = prep$scaling_factor
+# )
 data_list <- list(
   N = nrow(vb12_inad_lga),
-  NS = nrow(vb12_inad_lga_complete),
-  adm2_index = vb12_inad_lga_complete$lga_id,
-  p_hat = vb12_inad_lga_complete$vb12_inad_dir,
-  v_hat = vb12_inad_lga_complete$vb12_inad_dir_var,
-  d = vb12_inad_lga_complete$d, 
-  k = vb12_inad_lga_complete$k,
+  NS = nrow(vb12_inad_lga_complete2),
+  adm2_index = vb12_inad_lga_complete2$lga_id,
+  p_hat = vb12_inad_lga_complete2$vb12_inad_wdir,
+  v_hat = vb12_inad_lga_complete2$vb12_inad_wdir_var,
+  d = vb12_inad_lga_complete2$degf, 
+  k = vb12_inad_lga_complete2$n_obs,
   N_edges = length(prep$n1),
   node1 = prep$n1,
   node2 = prep$n2,
   scaling_factor = prep$scaling_factor
 )
+
 # MCMC
 fit <- mod$sample(
   data = data_list,
@@ -146,8 +187,8 @@ fit <- mod$sample(
   refresh = 50 # print update every 50 iters
 )
 
-
 fit$save_output_files(dir = here('outputs','mcmc'), basename = "stan_postsample_BYM2")
+
 post_samples = fit$draws(format = "df",  inc_warmup = F)
 post_samples$chain = as.character(post_samples$.chain)
 colnames(post_samples) <- colnames(post_samples) %>%
@@ -164,7 +205,8 @@ for (param in colnames(post_samples)[2:10]){
 }
 
 df_p = fit$draws(format = "df", variables=c('p'),  inc_warmup = F) |> mutate(chain=as.character(.chain)) 
-df_v= fit$draws(format = "df", variables=c('v'),  inc_warmup = F) |>mutate(chain=as.character(.chain))
+df_v = fit$draws(format = "df", variables=c('v'),  inc_warmup = F) |>mutate(chain=as.character(.chain))
+
 colnames(df_p) <- colnames(df_p) %>%
   gsub("\\[", "", .) %>%
   gsub("\\]", "", .) %>%
@@ -183,8 +225,8 @@ for (param in colnames(df_p)[1:10]){
 }
 
 
-vb12_inad_lga$v12_inad_post_mean <- df_p[,1:nrow(vb12_inad_lga)] |> colMeans() |> unname()
-vb12_inad_lga$v12_inad_post_var <-  sapply(df_p[,1:nrow(vb12_inad_lga)],var) |> unname()
+vb12_inad_lga$vb12_inad_post_mean <- df_p[,1:nrow(vb12_inad_lga)] |> colMeans() |> unname()
+vb12_inad_lga$vb12_inad_post_var <-  sapply(df_p[,1:nrow(vb12_inad_lga)],var) |> unname()
 for (param in colnames(df_v)[1:10]){
   dat = df_v
   gg = ggplot(data=dat, aes_string(x=".iteration", y = param, color="chain"))+
@@ -231,26 +273,39 @@ naive_map
 # tmap_save(naive_map, "outputs/maps/vb12_inad_naive.png", width = 8, height = 8,
 #           units = "in", dpi = 600)
 
-direct_map <- plot_map(data = vb12_inad_lga, 
+direct_map <- plot_map(data = vb12_inad_lga , 
                        col = "vb12_inad_dir", 
                        title = "Vitamin B12 (design-based estimates)", 
                        metric = "Prevalence of inadequate intake (%)", 
                        level = "lga")
 
 direct_map
-
 # tmap_save(direct_map, "outputs/maps/vb12_inad_direct.png", width = 8, height = 8,
 #           units = "in", dpi = 600)
 
 
-FB_map <- plot_map(data = vb12_inad_lga ,
-                       col = "v12_inad_post_mean", 
-                       title = "Vitamin B12 (Full-Bayes BYM2 smoothed estimates)", 
+vb12_inad_lga_tmp <- vb12_inad_lga
+vb12_inad_lga_tmp$vb12_inad_wdir[vb12_inad_lga$degf==0|vb12_inad_lga$vb12_inad_wdir_var<1e-10] = NA
+vb12_inad_lga_tmp$vb12_inad_wdir = vb12_inad_lga_tmp$vb12_inad_wdir*100
+direct_map2 <- plot_map(data = vb12_inad_lga_tmp , 
+                       col = "vb12_inad_wdir", 
+                       title = "Vitamin B12 (design-based estimates)", 
                        metric = "Prevalence of inadequate intake (%)", 
                        level = "lga")
 
+direct_map2
+tmap_save(direct_map2, "outputs/maps/vb12_inad_direct_01rm.png", width = 8, height = 8,
+          units = "in", dpi = 600)
+
+
+FB_map <- plot_map(data = vb12_inad_lga ,
+                     col = "vb12_inad_post_mean", 
+                     title = "Vitamin B12 (Full-Bayes BYM2 smoothed estimates)", 
+                     metric = "Prevalence of inadequate intake (%)", 
+                     level = "lga")
+
 FB_map
-tmap_save(FB_map, "outputs/maps/vb12_inad_FB_BYM2.png", width = 8, height = 8,
+tmap_save(FB_map, "outputs/maps/vb12_inad_FB_BYM2_weighted.png", width = 8, height = 8,
           units = "in", dpi = 600)
 
 plot_map <- function(data, col, title, metric, level) {
@@ -297,4 +352,125 @@ vb12_var_smoothed_FB <- plot_map(data = vb12_inad_lga,
 vb12_var_smoothed_FB
 
 
+#-------------------------------------------------------------------------------
+alpha = 0.1
 
+vb12_inad_lga$vb12_inad_wdir_lower90_wilson <- wilson_lower(vb12_inad_lga$vb12_inad_wdir, vb12_inad_lga$n_eff,alpha = alpha)
+vb12_inad_lga$vb12_inad_wdir_upper90_wilson <- wilson_upper(vb12_inad_lga$vb12_inad_wdir, vb12_inad_lga$n_eff,alpha = alpha)
+vb12_inad_lga$vb12_inad_wdir_ci_length90_wilson <- vb12_inad_lga$vb12_inad_wdir_upper90_wilson - vb12_inad_lga$vb12_inad_wdir_lower90_wilson 
+mean(vb12_inad_lga$vb12_inad_wdir_ci_length90_wilson, na.rm = T)
+vb12_inad_lga$vb12_inad_wdir_lower90_wald <- (vb12_inad_lga$vb12_inad_wdir - qnorm(1-alpha/2)*sqrt(vb12_inad_lga$vb12_inad_wdir_var))
+vb12_inad_lga$vb12_inad_wdir_upper90_wald <- (vb12_inad_lga$vb12_inad_wdir + qnorm(1-alpha/2)*sqrt(vb12_inad_lga$vb12_inad_wdir_var))
+vb12_inad_lga$vb12_inad_wdir_ci_length90_wald<- vb12_inad_lga$vb12_inad_wdir_upper90_wald - vb12_inad_lga$vb12_inad_wdir_lower90_wald 
+mean(vb12_inad_lga$vb12_inad_wdir_ci_length90_wald, na.rm = T)
+
+df_quantile =  apply(df_p[,1:nrow(vb12_inad_lga)], 2 , quantile , probs = c(alpha/2,1-alpha/2) , na.rm = TRUE ) |> t()
+vb12_inad_lga$vb12_inad_post_lower90 <- df_quantile[,1]
+vb12_inad_lga$vb12_inad_post_upper90 <- df_quantile[,2]
+vb12_inad_lga$vb12_inad_post_ci_length90<- vb12_inad_lga$vb12_inad_post_upper90 - vb12_inad_lga$vb12_inad_post_lower90 
+mean(vb12_inad_lga$vb12_inad_post_ci_length90, na.rm = T)
+#-------------------------------------------------------------------------------
+vb12_inad_lga$int_overlap <-check_overlap(vb12_inad_lga$vb12_inad_wdir_lower90_wald,vb12_inad_lga$vb12_inad_wdir_upper90_wald,
+              vb12_inad_lga$vb12_inad_post_lower90,vb12_inad_lga$vb12_inad_post_upper90)
+mean(vb12_inad_lga$int_overlap[!vb12_inad_lga$vb12_inad_wdir%in%c(0.0,1.0)], na.rm=T)
+#-------------------------------------------------------------------------------
+# Get adm1 estimates 
+adm2_pop <- read_csv(here("processed_data","population_by_lga.csv"))
+adm2_pop <- adm2_pop |> group_by(adm1_name) |> 
+  reframe(lga = lga,lga_id = lga_id,pop_adm2 = pop_total,pop_adm1 = sum(pop_total))  |>
+  mutate(geo_weight = pop_adm2/pop_adm1) 
+
+# vb12_inad_lga <- vb12_inad_lga |> 
+#   left_join(adm2_pop |> mutate(lga_id2 = lga_id, state = adm1_name) |> select(state, lga, lga_id2, prop_pop_adm2) , by = 'lga')
+# #vb12_inad_lga <- vb12_inad_lga |> select(-state, -lga_id2, -pop_total)
+
+nga_analysis_df <- nga_analysis_df |> 
+  left_join((nga_analysis_df|>
+               group_by(state) |>
+               reframe(hhid = hhid,
+                       survey_wgt=survey_wgt,
+                       sum_wgt = sum(survey_wgt),
+                       norm_survey_wgt_state = survey_wgt/sum_wgt) |>
+               select(-state, -survey_wgt,-sum_wgt)),
+            by = 'hhid'
+  ) 
+
+
+vb12_inad_state <- nga_analysis_df |> filter(lga!="bakassi") |> group_by(state) |>
+  summarise(n_obs = n(),
+            n_eff = 1/sum(norm_survey_wgt_state^2),
+            degf = (n_eff-1),
+            vb12_inad_wdir =  weighted.mean(vb12_inadequate, norm_survey_wgt_state),
+            vb12_inad_wdir_var = vb12_inad_wdir*(1-vb12_inad_wdir)/degf
+            
+            )
+vb12_inad_state$vb12_inad_wdir_lower90_wilson <- wilson_lower(vb12_inad_state$vb12_inad_wdir, vb12_inad_state$n_eff,alpha = alpha)
+vb12_inad_state$vb12_inad_wdir_upper90_wilson <- wilson_upper(vb12_inad_state$vb12_inad_wdir, vb12_inad_state$n_eff,alpha = alpha)
+vb12_inad_state$vb12_inad_wdir_ci_length90_wilson <- vb12_inad_state$vb12_inad_wdir_upper90_wilson - vb12_inad_state$vb12_inad_wdir_lower90_wilson 
+mean(vb12_inad_state$vb12_inad_wdir_ci_length90_wilson, na.rm = T)
+vb12_inad_state$vb12_inad_wdir_lower90_wald <- (vb12_inad_state$vb12_inad_wdir - qnorm(1-alpha/2)*sqrt(vb12_inad_state$vb12_inad_wdir_var))
+vb12_inad_state$vb12_inad_wdir_upper90_wald <- (vb12_inad_state$vb12_inad_wdir + qnorm(1-alpha/2)*sqrt(vb12_inad_state$vb12_inad_wdir_var))
+vb12_inad_state$vb12_inad_wdir_ci_length90_wald<- vb12_inad_state$vb12_inad_wdir_upper90_wald - vb12_inad_state$vb12_inad_wdir_lower90_wald 
+mean(vb12_inad_state$vb12_inad_wdir_ci_length90_wald, na.rm = T)
+
+df_post <- cbind(vb12_inad_lga |> select(state,lga,lga_id),t(df_p[,1:nrow(vb12_inad_lga)])) |> 
+  left_join(adm2_pop |> mutate(lga_id2 = lga_id) |> select(lga, lga_id2, geo_weight) , 
+            by = 'lga') |> filter(lga!="bakassi")
+### ---> should remove lga not included in the original data and recalculate the population weights? 
+df_post_state <- df_post |> as.data.frame() |> select(-geometry) |> group_by(state) |>
+  summarise(across(all_of(colnames(df_post)[grepl('X',colnames(df_post))]), ~ weighted.mean(., w = geo_weight))) 
+df_post_state$vb12_inad_post_mean <- df_post_state[,2:nrow(df_p)] |> rowMeans()
+df_quantile =  apply(df_post_state[,2:nrow(df_p)], 1 , quantile , probs = c(alpha/2,1-alpha/2) , na.rm = TRUE ) |> t()
+df_post_state$vb12_inad_post_lower90 <- df_quantile[,1]
+df_post_state$vb12_inad_post_upper90 <- df_quantile[,2]
+df_post_state$vb12_inad_posr_ci_length90 <- df_post_state$vb12_inad_post_upper90 - df_post_state$vb12_inad_post_lower90 
+mean(df_post_state$vb12_inad_posr_ci_length90)
+vb12_inad_state  <- vb12_inad_state |> left_join((df_post_state|> select(-colnames(df_post)[grepl('X',colnames(df_post))])), by = 'state')
+
+mean(abs(vb12_inad_state$vb12_inad_wdir - vb12_inad_state$vb12_inad_post_mean))
+vb12_inad_state$int_overlap <-check_overlap(vb12_inad_state$vb12_inad_wdir_lower90_wald,vb12_inad_state$vb12_inad_wdir_upper90_wald,
+                                          vb12_inad_state$vb12_inad_post_lower90,vb12_inad_state$vb12_inad_post_upper90)
+mean(vb12_inad_state$int_overlap)
+vb12_inad_state$bias = vb12_inad_state$vb12_inad_post_mean - vb12_inad_state$vb12_inad_wdir 
+mean(abs(vb12_inad_state$bias))
+write_csv(vb12_inad_state, here("processed_data","state_level_estimates.csv"))
+#-------------------------------------------------------------------------------
+vb12_inad_state <- vb12_inad_state |> left_join(nigeria_1|> select(state, geometry), by='state')
+vb12_inad_state <- st_as_sf(vb12_inad_state)
+p = ggplot(vb12_inad_state |> st_as_sf()
+           , aes(fill = vb12_inad_post_mean)) +
+  geom_sf() +
+  #facet_wrap(~Age) +
+  scale_fill_gradientn(values = c("1","0.8","0.6","0.4","0.2","0"), 
+                       colors = c("red4","red3","#FC4E07","#E7B800","green4"),
+                       limits=c(0,1))+
+  labs(title='Full Bayse estimate',fill = "VB12 deficiency")+
+  theme_minimal(base_size = 14) 
+p
+#ggsave(here("outputs","maps",paste0("vb12_inad_state_FB.png")), p, width = 10, height = 10,bg = "white")
+
+p = ggplot(vb12_inad_state |> st_as_sf()
+           , aes(fill = vb12_inad_wdir)) +
+  geom_sf() +
+  #facet_wrap(~Age) +
+  scale_fill_gradientn(values = c("1","0.8","0.6","0.4","0.2","0"), 
+                       colors = c("red4","red3","#FC4E07","#E7B800","green4"),
+                       limits=c(0,1))+
+  labs(title='Direct estimate',fill = "VB12 deficiency")+
+  theme_minimal(base_size = 14) 
+p
+#ggsave(here("outputs","maps",paste0("vb12_inad_state_direct.png")), p, width = 10, height = 10,bg = "white")
+
+p = ggplot(vb12_inad_state |> st_as_sf()
+           , aes(fill = bias)) +
+  geom_sf() +
+  #facet_wrap(~Age) +
+  scale_fill_gradient2(  high = "tomato",
+                         mid = "white",
+                         low = "royalblue") +
+  # scale_fill_gradientn(values = c("-0.3","-0.15","0","0.15","0.3"), 
+  #                      colors = c("red4","red3","#FC4E07","#E7B800","green4"))+
+  labs(title='Bias')+
+  theme_minimal(base_size = 14) 
+p
+ggsave(here("outputs","maps",paste0("vb12_inad_state_bias.png")), p, width = 10, height = 10,bg = "white")
