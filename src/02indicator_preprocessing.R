f298bbabf09196734ba8ba040d54d0680d953168
Modelling and Mapping Inadequate Micronutrient Intake <HQ.MIMIGitHub@wfp.org>
2025-07-20 19:38:30 +0100

    Update 02indicator_preprocessing.R
    Process Rwanda indicators

--- src/02indicator_preprocessing.R
+++ src/02indicator_preprocessing.R
@@ -4,7 +4,7 @@
 
 # Author: Mo Osman
 # Date created: 19-09-2024
-# Last edited: 17-04-2024
+# Last edited: 20-07-2025
 
 # In this script, I will extract and pre-process indicators from the NLSS survey 
 # ready for analyses.
@@ -30,10 +30,11 @@ source("src/01helper_functions.R")
 #-------------------------------------------------------------------------------
 
 # Get base case apparent intake for each MN: 
-nga_base_ai <- apparent_intake("nga_lss1819")
+nga_base_ai <- apparent_intake("/nga_lss1819")
+rwa_base_ai <- apparent_intake("/rwa_eicv2324")
 
 # Tidy environment: 
-rm(list = setdiff(ls(), c("nga_base_ai", "allen_ear")))
+rm(list = setdiff(ls(), c("nga_base_ai","rwa_base_ai", "allen_ear")))
 
 #-------------------------------------------------------------------------------
 
@@ -44,16 +45,23 @@ nga_base_ai$vb12_inadequate <- ifelse(nga_base_ai$vitb12_mcg < allen_ear$ear_val
 nga_base_ai <- nga_base_ai %>% 
   dplyr::select(hhid, folate_inadequate, vb12_inadequate)
 
+# Same for Rwanda: 
+rwa_base_ai$folate_inadequate <- ifelse(rwa_base_ai$folate_mcg < allen_ear$ear_value[allen_ear$nutrient == "folate_mcg"], 1, 0)
+rwa_base_ai$vb12_inadequate <- ifelse(rwa_base_ai$vitb12_mcg < allen_ear$ear_value[allen_ear$nutrient == "vitb12_mcg"], 1, 0)
+
+rwa_base_ai <- rwa_base_ai %>% 
+  dplyr::select(hhid, folate_inadequate, vb12_inadequate)
 
 # For purpose of this proof-of-concept study we will focus on these micronutrients, 
 # however these methods can be replicated for any micronutrient.
 
 # Tidy environment: 
-rm(list = setdiff(ls(), c("nga_base_ai")))
+rm(list = setdiff(ls(), c("nga_base_ai", "rwa_base_ai")))
 
 ################################################################################
 ############################### END OF SCRIPT ##################################
 ################################################################################
+
 
 
 
